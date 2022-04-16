3556929ee550f5e9e5fd7fc980cb05046675ba61
Pontus <pontusbergqvist97@gmail.com>
2022-04-16 13:50:45 +0200

    Dynamic route for "/work" uses local json file instead

--- pages/work/[project].tsx
+++ pages/work/[project].tsx
@@ -1,7 +1,46 @@
 import Link from '../../components/shared/link';
+import data from '../../data/data.json';
 
-const Project = () => {
-	return <div><p>helloooo</p><Link to='/work'>back</Link></div>	
+interface params {
+	params: {
+		project: number
+	}
+} 
+
+interface props {
+	data: {
+		id: number,
+		name: string,
+		description: string,
+		url: string	
+	}
+}
+
+
+export const getStaticPaths = () => {
+	const paths = data.map(item => ({
+		params: {
+			project: item.id.toString(),
+		}
+	})) 
+
+  return {
+		paths,
+		fallback: false
+	}
+}
+
+export const getStaticProps = ({ params }: params) => {
+	const obj = data.find(item => item.id == params.project)
+	return {
+		props: {
+			data: obj
+	  }
+	}
+}
+
+const Project = ({ data }: props) => {
+	return <div><p>{data.name} {data.id + 1}</p><Link to='/work'>back</Link></div>	
 }
 
 export default Project;
--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -2,9 +2,10 @@ import Head from 'next/head';
 import Link from 'next/link';
 import type { NextPage } from 'next';
 import Heading from '../../components/shared/heading';
+import data from '../../data/data.json';
 
 const Work: NextPage = () => {
-	const arr = [0, 1, 2, 3];
+
 	return (
 		<>
 			<Head>
@@ -13,12 +14,12 @@ const Work: NextPage = () => {
 			<div className='my-28'>
 				<Heading>Portfolio</Heading>
 				<div className='grid grid-cols-2 gap-8'>
-				{arr.map(item => (
-					<Link href={`/work/${item}`} key={item}>
+				{data.map(item => (
+					<Link href={`/work/${item.id}`} key={item.id}>
 						<article className='text-center cursor-pointer'>
-							<img className="h-[125px] w-full object-none rounded mb-2" src="https://images.unsplash.com/photo-1649899029737-7417f2b9d947?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80" alt="asdf"/>
+							<img className="h-[125px] w-full object-none rounded mb-2" src={item.url} alt="asdf"/>
 							<h3 className='text-h3'>Project</h3>
-							<p className='text-sm my-1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							<p className='text-sm my-1'>{item.description}</p>
 						</article>
 					</Link>
 				))}

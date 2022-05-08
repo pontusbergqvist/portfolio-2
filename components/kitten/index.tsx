3e9e80e402159f1aa7bc733d9c21871fe03f0558
Pontus <pontusbergqvist97@gmail.com>
2022-05-08 16:40:32 +0200

    client side rendering footer ascii art

--- components/kitten/index.tsx
+++ components/kitten/index.tsx
@@ -6,7 +6,7 @@ const Kitten = () => {
 	const [ref, isHovering] = useHover();
 	const ssr = useSsr();
 
-	return !ssr && (
+	return !ssr ? (
 		<a href='mailto:pontus@bergqvist.io' ref={ref} className="pointer-events-none blog:pointer-events-auto relative font-mono p-5 cursor-pointer self-end footer:self-auto">
 		&nbsp;/| ､ 		 <br />
 				(°､ ｡ 7 	 <br />
@@ -14,7 +14,7 @@ const Kitten = () => {
 				 じしf_,)〳<br />
 				{isHovering && <Typewriter>Get in touch!</Typewriter>}
 		</a>	
-	)
+	) : ""
 }
 
 export default Kitten;

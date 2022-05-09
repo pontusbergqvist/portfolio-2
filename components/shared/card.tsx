ea95507a4b92e994c74c309c84ffe4c751109d81
Pontus <pontusbergqvist97@gmail.com>
2022-05-09 15:18:07 +0200

    Fixed so card links doesnt scroll to top on click

--- components/shared/card.tsx
+++ components/shared/card.tsx
@@ -25,7 +25,7 @@ const variants = {
 const Card = ({ data, home, work, blog }: Props) => {
 	const { url, date, name, description, id } = data;
 	return (
-		<Link href={`/${ work ? "work" : blog && "blog" }/${id}`} passHref>
+		<Link href={`/${ work ? "work" : blog && "blog" }/${id}`} passHref scroll={false}>
 			<motion.article whileHover="hover" className="hover:cursor-pointer h-full w-full overflow-hidden">
 				<div className="overflow-hidden h-[150px] max-w-[270px] blog:max-w-full rounded mx-auto">
 					<motion.img variants={variants} className="align-middle h-full w-full object-cover mb-2" src={url} alt={name} />

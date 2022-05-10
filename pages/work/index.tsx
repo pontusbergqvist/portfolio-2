a5de73a00a001aa095063cd751e4958c3c8648da
Pontus <pontusbergqvist97@gmail.com>
2022-05-10 22:53:45 +0200

    temporary fix

--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -1,7 +1,6 @@
 import Layout from '../../components/layout';
 import type { NextPage } from 'next';
 import Heading from '../../components/shared/heading';
-import data from '../../data/data.json';
 import Card from '../../components/shared/card';
 
 const Work: NextPage = () => {
@@ -10,7 +9,6 @@ const Work: NextPage = () => {
 			<div className='my-8 md:my-28 max-w-[290px] blog:max-w-full mx-auto'>
 				<Heading>Work</Heading>
 				<div className='grid grid-cols-1 blog:grid-cols-2 gap-8'>
-				{data.map((item, index) => <Card work key={index} data={item} />)} 
 				</div>
 			</div>
 		</Layout>

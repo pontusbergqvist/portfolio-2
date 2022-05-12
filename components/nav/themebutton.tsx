38dd123e25c1f4628d75778b6bff70a51d7724ad
Pontus <pontusbergqvist97@gmail.com>
2022-05-12 22:08:36 +0200

    testing a smaller theme button

--- components/nav/themebutton.tsx
+++ components/nav/themebutton.tsx
@@ -10,16 +10,17 @@ const ThemeButton = () => {
 	return !ssr ? (
 				<div
           aria-hidden
-          className="z-10 relative hidden md:flex items-center w-[60px] h-[28px] bg-dark border-2 border-dark rounded-full cursor-pointer dark:border-light light:bg-dark"
+          className="z-10 relative hidden md:flex items-center w-[50px] h-[24px] bg-dark border-2 border-dark rounded-full cursor-pointer dark:border-light light:bg-dark"
           onClick={() => theme === 'dark' ? setTheme('light') : setTheme('dark')}>
-          <div className='absolute top-1/2 translate-y-[-50%] left-[31px] text-yellow-400 dark:translate-x-[-22px]'>
+          <div className='absolute top-1/2 translate-y-[-50%] left-[26px] text-yellow-400 dark:translate-x-[-21px] scale-[0.8]'>
             {theme === 'dark' ? <BsFillSunFill /> : <BsFillMoonFill />}
           </div>
           <div
-            className='absolute h-[28px] w-[28px] border-2 left-[-2px] bg-light border-dark rounded-full top-1/2 translate-y-[-50%] transition-transform duration-300 ease-out dark:translate-x-[32px] dark:bg-dark dark:border-light'
+            className='absolute h-[24px] w-[24px] border-2 left-[-2px] bg-light border-dark rounded-full top-1/2 translate-y-[-50%] transition-transform duration-300 ease-out dark:translate-x-[26px] dark:bg-dark dark:border-light'
           ></div>
         </div>
-	) : <div></div>;	
+			)
+	 : <div></div>;	
 }
 
 export default ThemeButton;

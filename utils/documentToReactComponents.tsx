edc61a52f3f09a92c4dd7df2ffa87e600a96496f
Pontus <pontusbergqvist97@gmail.com>
2022-05-17 13:04:06 +0200

    added types

--- utils/documentToReactComponents.tsx
+++ utils/documentToReactComponents.tsx
@@ -2,18 +2,18 @@ import { BLOCKS } from "@contentful/rich-text-types";
 
 const options = {
 	renderNode: {
-		[BLOCKS.PARAGRAPH]: (node, children) => (
+		[BLOCKS.PARAGRAPH]: (node, children): any => (
 			<p className="my-2">
 				{children}
 			</p>
 		),
-		[BLOCKS.HEADING_6]: (node, children) => (
+		[BLOCKS.HEADING_6]: (node, children): any => (
 			<span className="text-accent">{children}</span>
 		),
-		[BLOCKS.UL_LIST]: (node, children) => (
+		[BLOCKS.UL_LIST]: (node, children): any => (
 			<ul style={{ listStyle: "disc", marginLeft: 18}}>{children}</ul>
 		),
-		[BLOCKS.LIST_ITEM]: (node, children) => (
+		[BLOCKS.LIST_ITEM]: (node, children): any => (
 			<li>{children}</li>
 		)
 	}

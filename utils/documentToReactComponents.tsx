9ddb8e1f4589dd391086c76e574253dda935c0f9
Pontus <pontusbergqvist97@gmail.com>
2022-05-17 15:04:43 +0200

    Needs unnused node param to work and types are "any". Might revisit this in the futute

--- utils/documentToReactComponents.tsx
+++ utils/documentToReactComponents.tsx
@@ -2,18 +2,18 @@ import { BLOCKS } from "@contentful/rich-text-types";
 
 const options = {
 	renderNode: {
-		[BLOCKS.PARAGRAPH]: (children: any) => (
+		[BLOCKS.PARAGRAPH]: (node: any, children: any) => (
 			<p className="my-2">
 				{children}
 			</p>
 		),
-		[BLOCKS.HEADING_6]: (children: any) => (
+		[BLOCKS.HEADING_6]: (node: any, children: any) => (
 			<span className="text-accent">{children}</span>
 		),
-		[BLOCKS.UL_LIST]: (children: any) => (
+		[BLOCKS.UL_LIST]: (node: any, children: any) => (
 			<ul style={{ listStyle: "disc", marginLeft: 18}}>{children}</ul>
 		),
-		[BLOCKS.LIST_ITEM]: (children: any)  => (
+		[BLOCKS.LIST_ITEM]: (node: any, children: any)  => (
 			<li>{children}</li>
 		)
 	}

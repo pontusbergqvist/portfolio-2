fbe6810b8cb2f710594f1bbeaaa128a37c5d7504
Pontus <pontusbergqvist97@gmail.com>
2022-05-19 15:53:20 +0200

    changed header text content

--- components/home/header.tsx
+++ components/home/header.tsx
@@ -1,3 +1,5 @@
+import { GoLocation } from 'react-icons/go';
+
 const Header = () => {
 	return (
 			<header className="my-28">
@@ -6,7 +8,7 @@ const Header = () => {
 					<div className="mx-5">
 						<h1 className="text-h1 font-mono">Pontus Bergqvist</h1>
 						<p className="text-white text-h3 py-1 px-2 bg-accent inline">Full-stack web developer</p>
-						<p className="mt-2 text-sm">Not just a web developer, anyway.</p>
+						<p className="flex items-center font-mono my-1"><GoLocation /> <span className="ml-2">Gothenburg, Sweden</span></p>
 					</div>
 				</div>
 			</header>

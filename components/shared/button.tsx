dc076567b0908bd5ab8e4a12b4cd32ff116069bf
Pontus <pontusbergqvist97@gmail.com>
2022-05-19 12:20:13 +0200

    fixed type error

--- components/shared/button.tsx
+++ components/shared/button.tsx
@@ -8,7 +8,7 @@ interface Props {
 	type: string;
 	route: string;
 	children: string;
-	setDir: Dispatch<SetStateAction<string | undefined>>;
+	setDir: Dispatch<SetStateAction<string>>;
 }
 
 const Button = ({ data, type, route, children, setDir }: Props) => {

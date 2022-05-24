576087fdf9094743475cb7db9a13e649ab2da5f2
Pontus <pontusbergqvist97@gmail.com>
2022-05-24 17:00:28 +0200

    added 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,19 @@
+const Custom404 = () => {
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-y-1/2">
+      <div className="font-mono relative">
+        &nbsp;/| ､ <br />
+        (°､ ｡ 7 <br />
+        |､ ~ヽ <br />
+        じしf_,)〳
+        <br />
+        <span className="absolute top-[-55%] left-[-120%] text-accent">
+          404, page not found. Try again!
+        </span>
+        <span className="absolute top-0 -left-5 text-accent">\</span>
+      </div>
+    </div>
+  );
+};
+
+export default Custom404;

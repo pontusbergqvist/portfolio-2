af807ee3acbb80c28b354bed5d2fa503b140b5e8
Pontus <pontusbergqvist97@gmail.com>
2023-01-21 22:03:57 +0100

    updated info

--- components/home/about.tsx
+++ components/home/about.tsx
@@ -7,7 +7,7 @@ const About = () => {
       <main>
         <Heading>About</Heading>
         <p className="indent-4 mt-2">
-          I am a full-stack web developer from Gothenburg, Sweden. Currently I
+          I a a full-stack web developer from Gothenburg, Sweden. Currently I
           work as a front-end developer at
           <span className="text-accent"> Consid</span> in Ljungby. My favorite
           technologies include <span className="text-accent">NextJS</span>,{' '}
@@ -20,9 +20,9 @@ const About = () => {
           <h3 className="text-h3 my-1">Work</h3>
           <ul>
             <li className="bg-item-light p-2 my-3 first:mt-0 rounded dark:bg-item-dark">
-              <p className="font-mono">2019 - </p>
-              <p className="text-h3">Service technician</p>
-              <p>ABB</p>
+              <p className="font-mono">2023 - </p>
+              <p className="text-h3">Front-end developer</p>
+              <p>Consid</p>
             </li>
           </ul>
         </article>
